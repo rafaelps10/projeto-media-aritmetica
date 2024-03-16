b91d4c886cf9542d7d8496244c46bc925838f63b
Rafael P. Santos <rafael.ps.skt@gmail.com>
2024-03-16 10:47:48 -0300

    projeto realizado

---
+++ OneDrive/Área de Trabalho/Media/index.js
@@ -0,0 +1,10 @@
+function solucao(lista) {
+    let soma = 0;
+  
+    for (let i = 0; i < lista.length; i++) {
+        let numero = lista[i] / lista.length;
+        soma += numero;
+    }
+  
+  console.log(soma);
+}
